62149d8d81a69845931d559ad221779b74d9f384
Naywia <48431873+Naywia@users.noreply.github.com>
2023-11-14 14:45:32 +0100

    Create LoginView

---
+++ HolidayPlanner/Views/LoginView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace HolidayPlanner.Views
+{
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
