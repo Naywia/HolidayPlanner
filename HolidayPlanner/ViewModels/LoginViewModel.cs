7423ba9de1d7d598b982b4ecd92d8714632df12f
Naywia <48431873+Naywia@users.noreply.github.com>
2023-12-03 13:52:17 +0100

    Begun on Login

---
+++ HolidayPlanner/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayPlanner.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        public void Login(string username, string password)
+        {
+
+        }
+    }
+}
