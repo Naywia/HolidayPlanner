4fd425c8fea6329e9019b8b894f6e23791390910
Naywia <48431873+Naywia@users.noreply.github.com>
2023-12-04 16:03:25 +0100

    Added INotifyPropertyChanged

--- HolidayPlanner/ViewModels/ViewModelBase.cs
+++ HolidayPlanner/ViewModels/ViewModelBase.cs
@@ -1,7 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace HolidayPlanner.ViewModels;
 
-public class ViewModelBase : ObservableObject
+public abstract class ViewModelBase : ObservableObject, INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
