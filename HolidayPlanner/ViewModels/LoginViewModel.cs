663c79fd0b7a52fe32d5fcd922ab5d292da77583
Naywia <48431873+Naywia@users.noreply.github.com>
2023-12-04 16:02:53 +0100

    Added Properties

--- HolidayPlanner/ViewModels/LoginViewModel.cs
+++ HolidayPlanner/ViewModels/LoginViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,53 @@ namespace HolidayPlanner.ViewModels
 {
     public class LoginViewModel : ViewModelBase
     {
-        public void Login(string username, string password)
-        {
+        private string username;
+        private string username_temp = "oln";
+        private string password = "temp";
+        private string errorMsg = string.Empty;
+        //public ReactiveCommand<string, Unit> LoginCommand { get; }
 
+        //public LoginViewModel()
+        //{
+        //    LoginCommand = ReactiveCommand.Create<string>(Login);
+        //}
+
+        public void Login(string password)
+        {
+            if ((Username == username_temp) && (password == this.password))
+            {
+                // Set viewmodel here.
+            }
+            else
+            {
+                ErrorMsg = "Forkert login";
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string ErrorMsg
+        {
+            get
+            {
+                return errorMsg;
+            }
+            set
+            {
+                errorMsg = value;
+                NotifyPropertyChanged();
+            }
         }
     }
 }
