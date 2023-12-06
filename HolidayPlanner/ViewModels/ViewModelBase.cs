0d994a3a5cc420e05c9ca02cf6eb4cfd4e476dd4
Naywia <48431873+Naywia@users.noreply.github.com>
2023-12-06 13:58:37 +0100

    Update ViewModelBase.cs

--- HolidayPlanner/ViewModels/ViewModelBase.cs
+++ HolidayPlanner/ViewModels/ViewModelBase.cs
@@ -1,10 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ReactiveUI;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace HolidayPlanner.ViewModels;
 
-public abstract class ViewModelBase : ObservableObject, INotifyPropertyChanged
+public abstract class ViewModelBase : ReactiveObject, INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler PropertyChanged;
     protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
