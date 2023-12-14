c6a6d4786731eab767ee9d1307bd4f0ece0315b2
RasmusNorby <rasmus.norby@gmail.com>
2023-12-14 09:47:34 +0100

    Added repo instantiation

--- HolidayPlanner/ViewModels/MainViewModel.cs
+++ HolidayPlanner/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Interactivity;
+using HolidayPlanner.Models;
 using System;
 
 namespace HolidayPlanner.ViewModels
@@ -6,5 +7,9 @@ namespace HolidayPlanner.ViewModels
     public class MainViewModel : ViewModelBase
     {
         public static readonly Action<Object?, RoutedEventArgs> Thing = (sender, e) => { };
+
+        public static VacationTypeRepo vacationTypeRepo = new();
+        public static VacationRequestRepo VacationRequestRepo = new(vacationTypeRepo);
+        public static EmployeeRepo employeeRepo = new(VacationRequestRepo);
     }
 }
