5489a8b2aebaede376b63681248c8c15caa42cd1
Naywia <48431873+Naywia@users.noreply.github.com>
2023-12-14 09:08:51 +0100

    Add password to employee

--- HolidayPlanner/Models/Employee.cs
+++ HolidayPlanner/Models/Employee.cs
@@ -6,13 +6,15 @@ public class Employee {
     public int Id {get; private set;}
     public string Email {get; set;}
     public string Username {get; set;}
+    public string Password { get; private set; }
     public double AvailableVacationDays {get; set;}
     public List<VacationRequest> VacationRequests {get; private set;}
 
-    public Employee (int Id, string Email, string Username, double AvailableVacationDays, List<VacationRequest> VacationRequests) {
+    public Employee (int Id, string Email, string Username, string Password, double AvailableVacationDays, List<VacationRequest> VacationRequests) {
         this.Id = Id;
         this.Email = Email;
         this.Username = Username;
+        this.Password = Password;
         this.AvailableVacationDays = AvailableVacationDays;
         this.VacationRequests = VacationRequests;
     }
@@ -21,6 +23,7 @@ public class Employee {
         this.Id = oldEmployee.Id;
         this.Email = new string(oldEmployee.Email);
         this.Username = new string(oldEmployee.Username);
+        this.Password = new string(oldEmployee.Password);
         this.AvailableVacationDays = oldEmployee.AvailableVacationDays;
         this.VacationRequests = oldEmployee.VacationRequests;
     }
